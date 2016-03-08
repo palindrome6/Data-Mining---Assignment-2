23df28ec06c5779fad0b9974bb961054bb67e9ab
Nitin Narayan <nitinnarayan1501@gmail.com>
2016-03-08 15:05:43 +0100

    test1

---
+++ testcode1.py
@@ -0,0 +1,8 @@
+__author__ = 's158079'
+from openml.apiconnector import APIConnector
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+from pandas.tools.plotting import parallel_coordinates
