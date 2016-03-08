a493f7534357fa675a844631346ef930d114f758
Nitin Narayan <nitinnarayan1501@gmail.com>
2016-03-08 14:49:06 +0100

    Test

--- r_iris.R
+++ r_iris.R
@@ -16,17 +16,17 @@ library(partykit)
 
 iris = getOMLDataSet(did = 10, verbosity=0)$data
 
-iris<-iris[!(iris$class=="fibrosis" | iris$class=="normal"),]
+# iris<-iris[!(iris$class=="fibrosis" | iris$class=="normal"),]
 iristask = makeClassifTask(data = iris, target = "class")
 iris_mat = data.matrix(iris)
 # ggplot(data=iris, aes(x=sepallength, y=sepalwidth, color=class, shape=class)) + geom_point(size=2) + theme_cowplot() + theme(legend.position = c(0.14, 0.80))
 
-ab = iris[,1:18]
-cd = iris[,19]
-
-str(iris)
-m1 <- J48(class~., data = iris)
-# if(require("party", quietly = TRUE)) plot(m1)
+# ab = iris[,1:18]
+# cd = iris[,19]
+# 
+# str(iris)
+# m1 <- J48(class~., data = iris)
+# # if(require("party", quietly = TRUE)) plot(m1)
 
 
 rpart = makeLearner("classif.rpart")
