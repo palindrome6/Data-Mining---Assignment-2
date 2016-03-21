89ce65a09100cfcdd02931a929cb5352fd939375
Rodrigo Mendoza <rmendoza@mendozablanco.com.mx>
2016-03-21 22:22:16 +0100

    ipynb and some changes on model selection

--- roc_curves/model_selection.py
+++ roc_curves/model_selection.py
@@ -1,60 +1,128 @@
 from sklearn import cross_validation
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.cross_validation import KFold
+from sklearn.cross_validation import KFold, Bootstrap, ShuffleSplit
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.cross_validation import Bootstrap
+from sklearn.metrics import r2_score, mean_squared_error, make_scorer
+from sklearn.cross_validation import cross_val_score
+from sklearn.ensemble import BaggingClassifier
+from sklearn.metrics import zero_one_loss, accuracy_score, mean_squared_error
 
 def exercise_1():
-    X, y = make_blobs(n_samples=1000, centers=2, n_features=2,
-                      random_state=0)
-    accuracy_lst = np.zeros([49, 2], dtype=float)
-    for i in range(0,49):
-        clf = KNeighborsClassifier(n_neighbors=i+1)
-        scores = cross_validation.cross_val_score(
-           clf, X, y, cv=10, scoring='mean_absolute_error')
-        accuracy = (scores.mean(), scores.std() * 2)
-        accuracy_lst[i,0] = accuracy[0]
-        accuracy_lst[i,1] = accuracy[1]
-    #plot
-    accuracy_lst = np.around(accuracy_lst, decimals=5)
+    X, y = make_blobs(n_samples=1000,centers=50, n_features=2, random_state=0)
+    n_samples = len(X)
+    kf = cross_validation.KFold(n_samples, n_folds=10, shuffle=False, random_state=None)
+    # kf = cross_validation.ShuffleSplit(1000,n_iter=25, test_size=0.1, train_size=0.9, random_state=None)
+
+    error_total = np.zeros([49, 1], dtype=float)
+    for k in range(1,50):
+        error = []
+        clf = KNeighborsClassifier(n_neighbors=k)
+        for train_index, test_index in kf:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            clf.fit(X_train, y_train)
+            error.append( zero_one_loss(y_test, clf.predict(X_test)) )
+
+
+            # error.append(clf.predict(X_test))
+            # error.append( 1. - clf.score(X_test, y_test) ) #, accuracy_score(y_test, clf.predict(X_test))
+            # error.append(mean_squared_error(y_test, clf.predict(X_test)))
+            # error.append()
+        # print error
+        error_total[k-1, 0] = np.array(error).mean()
+    # print error_total
     x = np.arange(1,50, dtype=int)
-    y = accuracy_lst[:,0]
-    e = accuracy_lst[:,1]
     plt.style.use('ggplot')
-    plt.plot(x,y, 'ro')
+    plt.plot(x, error_total[:, 0], '#009999', marker='o')
+    # plt.errorbar(x, accuracy_lst[:, 0], accuracy_lst[:, 1], linestyle='None', marker='^')
     plt.xticks(x, x)
     plt.margins(0.02)
+    plt.xlabel('K values')
+    plt.ylabel('Missclasification Error')
     plt.show()
+    # print cross_validation.cross_val_score(KNeighborsClassifier(n_neighbors=3), X, y, cv=cv, scoring='zero_one_loss')
+    # print cross_validation.cross_val_score(KNeighborsClassifier(n_neighbors=3), X, y, cv=cv, scoring='accuracy')
 
-def exercise_2():
-    X, y = make_blobs(n_samples=1000, centers=2, n_features=2,
-                      random_state=0)
 
+def exercise_2a():
+    X, y = make_blobs(n_samples=1000,centers=50, n_features=2, random_state=0)
+    # plt.scatter(X[:, 0], X[:, 1], marker='o', c=y)
+    # plt.show()
+    kf = KFold(1000, n_folds=10, shuffle=False, random_state=None)
     accuracy_lst = np.zeros([49, 2], dtype=float)
-    for i in range(0,49):
-        clf = KNeighborsClassifier(n_neighbors=i+1)
+    accuracy_current = np.zeros(10, dtype=float)
+    for k in range(1,50):
+        iterator = 0
+        clf = KNeighborsClassifier(n_neighbors=k)
+        for train_index, test_index in kf:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
 
-        cv = cross_validation.ShuffleSplit(1000, n_iter=10,
-                                test_size=0.1, random_state=0)
-        scores = cross_validation.cross_val_score(
-           clf, X, y, cv=cv, scoring='mean_absolute_error')
-        accuracy = (scores.mean(), scores.std() * 2)
-        accuracy_lst[i,0] = accuracy[0]
-        accuracy_lst[i,1] = accuracy[1]
-    #plot
-    accuracy_lst = np.around(accuracy_lst, decimals=5)
+            clf.fit(X_train, y_train)
+            accuracy_current[iterator] = (1. - clf.score(X_test,y_test))
+            iterator+=1
+        accuracy_lst[k-1, 0] = accuracy_current.mean()
+        # accuracy_lst[k-1, 1] = accuracy_current.std() #confidence interval 95%
     x = np.arange(1,50, dtype=int)
-    y = accuracy_lst[:,0]
-    e = accuracy_lst[:,1]
     plt.style.use('ggplot')
-    plt.plot(x,y, 'ro')
+    plt.plot(x, accuracy_lst[:, 0], '#009999', marker='o')
+    # plt.errorbar(x, accuracy_lst[:, 0], accuracy_lst[:, 1], linestyle='None', marker='^')
     plt.xticks(x, x)
+    plt.margins(0.02)
+    plt.xlabel('K values')
+    plt.ylabel('Missclasification Error')
     plt.show()
+
+def exercise_2b():
+    X, y = make_blobs(n_samples=1000,centers=50, n_features=2, random_state=0)
+    kf = ShuffleSplit(100, train_size= 0.9, test_size=0.1, random_state=0)
+    # kf = KFold(1000, n_folds=10, shuffle=False, random_state=None)
+    accuracy_lst = np.zeros([49, 2], dtype=float)
+    accuracy_current = np.zeros(10, dtype=float)
+    for k in range(1,50):
+        iterator = 0
+        for train_index, test_index in kf:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            clf = KNeighborsClassifier(n_neighbors=k)
+            clf.fit(X_train, y_train)
+            accuracy_current[iterator] = (1. - clf.score(X_test,y_test))
+            iterator+=1
+            print mean_squared_error(y_test, clf.predict(X_test))
+        accuracy_lst[k-1, 0] = accuracy_current.mean()
+        accuracy_lst[k-1, 1] = accuracy_current.var()#*2 #confidence interval 95%
+    x = np.arange(1,50, dtype=int)
+    plt.style.use('ggplot')
+    plt.plot(x, accuracy_lst[:, 1], '#009999', marker='o')
+    # plt.errorbar(x, accuracy_lst[:, 0], accuracy_lst[:, 1], linestyle='None', marker='^')
+    plt.xticks(x, x)
+    plt.margins(0.02)
+    plt.xlabel('K')
+    plt.ylabel('Variance')
+    plt.show()
+
+
 
 if __name__ == "__main__":
-    exercise_2()
+    # exercise_2a()
+    exercise_1()
 
+
+    # X, y = make_blobs(n_samples=1000,centers=50, n_features=2, random_state=0)
+    # plt.scatter(X[:, 0], X[:, 1], marker='o', c=y)
+    # plt.show()
+    # folds = KFold(1000, n_folds=10, shuffle=False, random_state=None)
+    # missClassTotal = []
+    # for k in range(1,50):
+    #     yay = BaggingClassifier(KNeighborsClassifier(n_neighbors=k, weights='uniform', algorithm='auto', p=2, metric='minkowski'),max_samples=900, n_estimators=100, max_features=1)
+    #     yay.fit(X,y)
+    #     missClassTotal.append(1-yay.score(X,y))
+    # plt.plot(list(range(1,50)), missClassTotal)
+    # plt.axis([1, 49, 0, 1])
+    # plt.xlabel('k', fontsize=14, color='black')
+    # plt.ylabel('misclassification rate', fontsize=14, color='black')
+    # plt.show()
 
 
--- roc_curves/roc_curves_rod.py
+++ roc_curves/roc_curves_rod.py
@@ -9,16 +9,23 @@ from scipy import interp
 from sklearn.metrics import roc_auc_score
 from scipy.spatial import ConvexHull
 
-threshold = 0.5
+
+
+
+n_classes = 5
 y_true = np.array([1,1,1,1,1,1,1,0,0,0,0,0,0])
 scores_a = np.array([[1,1,0,0,1,1,0,0,1,0,0,0,0]])
 scores_b = np.array([[1,1,1,1,0,1,1,0,1,0,1,0,0]])
-scores_c = np.array([[0.8,0.9,0.7,0.6,0.4,0.8,0.4,0.4,0.6,0.4, 0.4, 0.4,0.2]])
-np.place(scores_c, scores_c > threshold, 1)
-np.place(scores_c, scores_c <= threshold, 0)
-scores = np.append(scores_a, scores_b, axis=0)
-scores = np.append(scores, scores_c, axis=0)
-n_classes = 3
+scores_c1 = np.array([[0.8,0.9,0.7,0.6,0.4,0.8,0.4,0.4,0.6,0.4, 0.4, 0.4,0.2]])
+scores_c2 = np.array([[0.8,0.9,0.7,0.6,0.4,0.8,0.4,0.4,0.6,0.4, 0.4, 0.4,0.2]])
+scores_c3 = np.array([[0.8,0.9,0.7,0.6,0.4,0.8,0.4,0.4,0.6,0.4, 0.4, 0.4,0.2]])
+
+scores_c1 = np.where(scores_c1>0.5,1,0)
+scores_c2 = np.where(scores_c2>0.3,1,0)
+scores_c3 = np.where(scores_c3>0.6,1,0)
+
+
+scores  = np.concatenate((scores_a,scores_b,scores_c1, scores_c2, scores_c3), axis=0)
 
 
 # Compute ROC curve and ROC area for each class
@@ -29,25 +36,46 @@ for i in range(n_classes):
     fpr[i], tpr[i], _ = roc_curve(y_true, scores[i], pos_label=1)
     roc_auc[i] = auc(fpr[i], tpr[i])
 
-print fpr[0]
-print tpr[0]
+# print fpr[1]
 #plot for each class
-names=['A','B','C']
+plt.style.use('ggplot')
+names = ['A','B','C1_0.5', 'C2_0.3', 'C3_0.6']
 for i in range(n_classes):
     plt.plot(fpr[i], tpr[i], label='ROC curve of class {0} (area = {1:0.4f})'
-                                   ''.format(names[i], roc_auc[i]))
-plt.plot([0, 1], [0, 1], 'k--')
+                                   ''.format(names[i], roc_auc[i]), linewidth=2.0)
+# plt.plot([0, 1], [0, 1], 'k')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
 plt.title('ROC curves')
 plt.legend(loc="lower right")
+# plt.show()
+
+print 0.85714286- (.2*0.33333333)
+print fpr[1]
+print tpr[1]
+
+# #convexHull
+points = np.vstack((fpr[0], tpr[0])).T
+for i in range(n_classes):
+    points = np.append(points, np.vstack((fpr[i], tpr[i])).T, axis=0)
+hull = ConvexHull(points)
+
+
+iterpoint = iter(hull.simplices)
+next(iterpoint)
+for simplex in iterpoint:
+    plt.plot(points[simplex, 0], points[simplex, 1], '#ff3300', lw=5, alpha=0.5)
+
+plt.plot(points[:,0], points[:,1], 'o', color = '#7a7a52', alpha=0.5)
+plt.plot([0,1],[0,1], 'k--')
+#margins
+plot_margin = 0.02
+x0, x1, y0, y1 = plt.axis()
+plt.axis((x0 - plot_margin,
+          x1 + plot_margin,
+          y0 - plot_margin,
+          y1 + plot_margin))
 plt.show()
-
-
-
-
-#convexHull
-
 
